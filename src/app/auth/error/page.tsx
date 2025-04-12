f5ebf0229d7e40621870f998dca54043f38aad3c
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2025-04-12 20:37:55 +0530

    added

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,8 +1,6 @@
-// app/auth/error/page.tsx
+
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
-
-// Dynamically import the actual client component
 const AuthErrorPageClient = dynamic(
   () => import("@/components/AuthErrorPage"),
   {
