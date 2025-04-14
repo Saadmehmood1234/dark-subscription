f994338d451b0777c1c601f14a54c22d59da1161
kamlesh-Sahani <alex20150001969@gmail.com>
2025-04-14 21:17:21 +0530

    changes

--- src/app/api/verify-payment/route.ts
+++ src/app/api/verify-payment/route.ts
@@ -2,13 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 import { Order } from "@/model/Order";
 
-export async function POST(request:NextRequest) {
-
-  const {sessionId,orderId} = await request.json();
+export async function POST(request: NextRequest) {
+  const { sessionId, orderId } = await request.json();
   // const { searchParams } = new URL(request.url);
   // const sessionId = searchParams.get("session_id");
   // const orderId = searchParams.get("order_id");
-console.log({sessionId,orderId})
+  console.log({ sessionId, orderId });
   if (!sessionId || !orderId) {
     return NextResponse.json(
       { success: false, error: "Missing parameters" },
@@ -18,22 +17,27 @@ console.log({sessionId,orderId})
 
   try {
     const session = await stripe.checkout.sessions.retrieve(sessionId);
-    console.log(session,"session")
     if (session.payment_status === "paid") {
-      console.log("check point")
-      const updatedOrder = await Order.findByIdAndUpdate(
-        orderId,
-        { paymentStatus: "paid", status: "delivered" },
-        { new: true }
-      )
+      const order = await Order.findById(orderId)
         .populate("userId")
         .populate("products.productId");
 
-      if (updatedOrder) {
+      if (!order) {
+        return NextResponse.json(
+          { success: false, error: "order is not found" },
+          { status: 400 }
+        );
+      }
+      order.paymentStatus="paid";
+      order.status="delivered";
+
+      await order.save();
+      if (order) {
         return NextResponse.json({
           success: true,
-          email: updatedOrder.userId.email,
-          productName: updatedOrder.products[0]?.productId?.title || "Subscription",
+          email: order.userId.email,
+          productName:
+          order.products[0]?.productId?.title || "Subscription",
         });
       }
     }
@@ -41,9 +45,9 @@ console.log({sessionId,orderId})
       { success: false, error: "Payment not verified" },
       { status: 400 }
     );
-  } catch (error:any) {
+  } catch (error: any) {
     return NextResponse.json(
-      { success: false, error:error.message || "Server error"  },
+      { success: false, error: error.message || "Server error" },
       { status: 500 }
     );
   }
