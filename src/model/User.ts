0dec177629dcfef87bc0a20ca31cb500d41ea4a3
Saad <mehmoodsaad347@gmail.com>
2026-07-31 18:15:06 +0530

    fix issue

--- src/model/User.ts
+++ src/model/User.ts
@@ -5,7 +5,7 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true, unique: true },
+    phone: { type: String },
     password: { type: String },
     image: {
       type: String,
